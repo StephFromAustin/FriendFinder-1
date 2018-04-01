72d91c12d5034b5f980be584faeb0dab3c5d3094
Daniel Doyle <dbdoyle182@gmail.com>
2018-04-01 15:34:38 -0400

    all routes are established and working

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+var friendArray = [
+    {
+        name: 'Daniel',
+        photo: 'http://via.placeholder.com/350x150',
+        scores: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    }
+];
+
+module.exports = friendArray;
+
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,13 @@
+// Dependencies
+var path = require('path');
+
+// Pull data 
+var friendData = require('../data/friends.js')
+
+// Exports the file paths
+module.exports = function(app){
+
+    app.get('/api/friends', function(req, res){
+        res.json(friendData);
+    });
+};
