c9a6fd43d42c3a5416b4c59d82d7d525c5a7bdda
Daniel Doyle <dbdoyle182@gmail.com>
2018-04-01 21:29:25 -0400

    working modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,16 +3,16 @@ var friendArray = [
         name: 'Daniel',
         photo: 'http://via.placeholder.com/350x150',
         scores: [
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1'
         ]
     }
 ];
